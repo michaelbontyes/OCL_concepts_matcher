e78f51d113ab599431645a2a89482275061f602b
Michaël Bontyes <michaelbontyes@gmail.com>
2024-06-25 16:05:09 -0400

    Lint Fixes

--- fetcher.py
+++ fetcher.py
@@ -1,14 +1,24 @@
-import requests
-import json
+"""
+Fetches all concepts from the OpenConceptLab API and returns them as a list.
+"""
 from datetime import datetime
+import json
+import requests
 
-def fetch_all_concepts(api_url):
+def fetch_all_concepts(url):
+    """
+        Fetches all concepts from the OpenConceptLab API and returns them as a list.
+    Args:
+        url (str): The URL of the API without the page parameter.
+    Returns:
+        list: A list of all concepts fetched from the API.
+    """
     total_concepts = 0
     page = 1
     concepts = []
 
     while True:
-        response = requests.get(f"{api_url}&page={page}")
+        response = requests.get(f"{url}&page={page}", timeout=5)
         if response.status_code == 200:
             data = response.json()
             concepts.extend(data)
@@ -26,17 +36,17 @@ def fetch_all_concepts(api_url):
     return concepts
 
 # URL of the API without the page parameter
-api_url = "https://api.openconceptlab.org/orgs/MSF/sources/MSF/concepts/?q=&limit=0"
+API_URL = "https://api.openconceptlab.org/orgs/MSF/sources/MSF/concepts/?q=&limit=0"
 
 # Fetch and print total concepts
-concepts = fetch_all_concepts(api_url)
-TOTAL_CONCEPTS = len(concepts)
+all_concepts = fetch_all_concepts(API_URL)
+TOTAL_CONCEPTS = len(all_concepts)
 print(f"Total number of concepts: {TOTAL_CONCEPTS}")
 
 # Save the fetched concepts to a JSON file with a timestamp
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 file_path = f"concepts_{timestamp}.json"
-with open(file_path, 'w') as file:
-    json.dump(concepts, file)
+with open(file_path, 'w', encoding='utf-8') as file:
+    json.dump(all_concepts, file)
 
 print(f"Concepts saved to {file_path}")
--- matcher.py
+++ matcher.py
@@ -1,14 +1,15 @@
-import pandas as pd
+""" Finds the best match for a given concept label with a strict threshold. """
 import json
+import pandas as pd
 from fuzzywuzzy import process
 
 # Load the CSV file
-csv_file_path = 'concepts.csv'
-csv_data = pd.read_csv(csv_file_path, encoding='ISO-8859-1')
+CSV_FILE_PATH = 'concepts.csv'
+csv_data = pd.read_csv(CSV_FILE_PATH, encoding='ISO-8859-1')
 
 # Load the JSON file
-json_file_path = 'concepts_20240625_150944.json'
-with open(json_file_path, 'r') as json_file:
+JSON_FILE_PATH = 'concepts_20240625_150944.json'
+with open(JSON_FILE_PATH, 'r', encoding='ISO-8859-1') as json_file:
     json_data = json.load(json_file)
 
 # Extract display names and external IDs from JSON data
@@ -17,25 +18,27 @@ external_ids = [item['external_id'] for item in json_data]
 
 # Function to find the best match for a given label with a stricter threshold
 def find_best_match(label, choices, threshold):
+    """ Finds the best match for a given label with a stricter threshold. """
     label = str(label)  # Convert label to string
     best_match, score = process.extractOne(label, choices)
     print(f"Label: {label}, Match: {best_match}, Score: {score}")
     if score >= threshold:
         return best_match, score
-    else:
-        return None, None
+    return None, None
 
 # Apply the updated function to the CSV data
-csv_data['Match'], csv_data['Score'] = zip(*csv_data['Label'].apply(lambda x: find_best_match(x, display_names, 95)))
+csv_data['Match'], csv_data['Score'] = zip(*csv_data['Label'].apply(
+    lambda x: find_best_match(x, display_names, 95)))
 
 # Remove rows where no match was found
 csv_data = csv_data.dropna(subset=['Match'])
 
 # Create a new column "External ID" in the CSV data
-csv_data['External ID'] = csv_data['Match'].apply(lambda x: external_ids[display_names.index(x)] if x is not None else '')
+csv_data['External ID'] = csv_data['Match'].apply(
+    lambda x: external_ids[display_names.index(x)] if x is not None else '')
 
 # Save the updated CSV file
-output_file_path = 'concepts_with_matches.csv'
-csv_data.to_csv(output_file_path, index=False)
+OUTPUT_FILE_PATH = 'concepts_with_matches.csv'
+csv_data.to_csv(OUTPUT_FILE_PATH, index=False)
 
-print(f"Updated CSV file saved as {output_file_path}")
+print(f"Updated CSV file saved as {OUTPUT_FILE_PATH}")
