85a13f94b7a0ee9156591c7bc5473709e48a00b5
Michaël Bontyes <michaelbontyes@gmail.com>
2024-06-26 12:49:31 -0400

    CIEL source snapshot

--- fetcher.py
+++ fetcher.py
@@ -1,52 +1,54 @@
-"""
-Fetches all concepts from the OpenConceptLab API and returns them as a list.
-"""
-from datetime import datetime
-import json
+import ijson
 import requests
+import json
+from datetime import datetime
 
-def fetch_all_concepts(url):
-    """
-        Fetches all concepts from the OpenConceptLab API and returns them as a list.
-    Args:
-        url (str): The URL of the API without the page parameter.
-    Returns:
-        list: A list of all concepts fetched from the API.
-    """
+def fetch_all_concepts(url, file_path):
     total_concepts = 0
     page = 1
-    concepts = []
+    is_first_page = True
 
     while True:
-        response = requests.get(f"{url}&page={page}", timeout=5)
+        response = requests.get(f"{url}&page={page}", timeout=30)  # Increased timeout duration to 30 seconds
         if response.status_code == 200:
             data = response.json()
-            concepts.extend(data)
             total_concepts += len(data)
 
             if not data:
                 break
 
+            with open(file_path, 'a', encoding='utf-8') as file:
+                if is_first_page:
+                    file.write("[\n")
+                    is_first_page = False
+                else:
+                    file.write(",\n")
+                
+                for i, concept in enumerate(data):
+                    json.dump(concept, file)
+                    if i < len(data) - 1:
+                        file.write(",\n")
+            
+            print(f"Total concepts found so far: {total_concepts}")
             page += 1
         else:
             print(f"Failed to fetch data. Status code: {response.status_code}")
             break
 
+    with open(file_path, 'a', encoding='utf-8') as file:
+        file.write("\n]")
+
     print(f"Total concepts found: {total_concepts}")
-    return concepts
+    return total_concepts
 
 # URL of the API without the page parameter
-API_URL = "https://api.openconceptlab.org/orgs/MSF/sources/MSF/concepts/?q=&limit=0"
-
-# Fetch and print total concepts
-all_concepts = fetch_all_concepts(API_URL)
-TOTAL_CONCEPTS = len(all_concepts)
-print(f"Total number of concepts: {TOTAL_CONCEPTS}")
+API_URL = "https://api.openconceptlab.org/orgs/CIEL/sources/CIEL/concepts/?q=&limit=0"
 
 # Save the fetched concepts to a JSON file with a timestamp
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 file_path = f"concepts_{timestamp}.json"
-with open(file_path, 'w', encoding='utf-8') as file:
-    json.dump(all_concepts, file)
 
+# Fetch and save total concepts incrementally
+total_concepts = fetch_all_concepts(API_URL, file_path)
+print(f"Total number of concepts: {total_concepts}")
 print(f"Concepts saved to {file_path}")
