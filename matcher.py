bdfe64d618352e961a803f3e6762ef7b506fb939
Michaël Bontyes <michaelbontyes@gmail.com>
2024-07-14 18:59:46 -0400

    Linting

--- matcher.py
+++ matcher.py
@@ -1,6 +1,7 @@
-import openpyxl
+"OCL Concepts Matcher to find existing concepts in OCL based on provided Excel metadata"
 import json
 import math
+import openpyxl
 from rapidfuzz import process, fuzz
 import pandas as pd
 
@@ -8,13 +9,13 @@ import pandas as pd
 openpyxl.reader.excel.warnings.simplefilter(action='ignore')
 
 # Excel Metadata filepath
-metadata_filepath = "LIME EMR - Iraq Metadata - Release 1 (18).xlsx"
+METADATA_FILEPATH = "LIME EMR - Iraq Metadata - Release 1 (18).xlsx"
 
 # OCL URL
 OCL_URL = "https://app.openconceptlab.org/#"
 
 # Matching threshold for fuzzy string matching
-FUZZY_THRESHOLD = 90
+FUZZY_THRESHOLD = 95
 
 # Initialize the counter of concept to match
 CONCEPTS_TO_MATCH = 0
@@ -24,12 +25,22 @@ MATCHES_FOUND = 0
 
 # Define the sheets to read
 #excel_sheets = [ 'FXX-Test_Only', 'F06-PHQ-9']
-excel_sheets = [ 'F01-MHPSS Baseline', 'F02-MHPSS Follow-up', 'F03-mhGAP Baseline', 'F04-mhGAP Follow-up', 'F05-MH Closure', 'F06-PHQ-9', 'F07-ITFC form', 'F08-ATFC form', 'OptionSets']
+excel_sheets = [
+    'F01-MHPSS Baseline', 
+    'F02-MHPSS Follow-up', 
+    'F03-mhGAP Baseline', 
+    'F04-mhGAP Follow-up', 
+    'F05-MH Closure', 
+    'F06-PHQ-9', 
+    'F07-ITFC form', 
+    'F08-ATFC form', 
+    'OptionSets'
+    ]
 
 # Define OCL sources and their respective columns in the Excel document
 automatch_references = {
     "MSF": {
-        "source_filepath": "MSF_Source_20240708_130415.json",
+        "source_filepath": "MSF_Source_20240712_163433.json",
         "suggestion_column": "MSF Source Suggestion",
         "external_id_column": "MSF Source External ID",
         "description_column": "MSF Source Description",
@@ -75,28 +86,26 @@ def find_best_matches(primary, secondary, data, threshold=FUZZY_THRESHOLD, limit
     display_names = [item['display_name'] for item in data]
 
     # Use rapidfuzz's process.extract to find the best matches with threshold
-    matches = process.extract(query, display_names, scorer=fuzz.WRatio, score_cutoff=threshold, limit=limit)
+    matches = process.extract(
+        query, display_names, scorer=fuzz.WRatio, score_cutoff=threshold, limit=limit
+        )
 
     # Map the matches back to their corresponding IDs and definitions
     results = []
-    for match, score, index in matches:
-        #print(f"Match: {match}, Score: {score}")
+    for _, score, match_index in matches:
         if score >= FUZZY_THRESHOLD:
             results.append((
-                data[index]['id'],
-                data[index]['external_id'],
-                data[index]['display_name'],
-                data[index]['description'],
-                data[index]['datatype'],
-                data[index]['concept_class'],
-                data[index]['url'],
+                data[match_index]['id'],
+                data[match_index]['external_id'],
+                data[match_index]['display_name'],
+                data[match_index]['description'],
+                data[match_index]['datatype'],
+                data[match_index]['concept_class'],
+                data[match_index]['url'],
                 score
                 ))
-            #print(json.dumps(results, indent=4))
-            global MATCHES_FOUND
-            MATCHES_FOUND += 1
-            return results
-    return []  # Return an empty list when there are no matches found above the threshold
+    # Return the list of results
+    return results
 
 # Open the metadata Excel file and find the column indices for the required columns
 def find_column_index(worksheet, column_name):
@@ -115,24 +124,34 @@ def find_column_index(worksheet, column_name):
             return idx
     return -1  # Return -1 if the column name is not found
 
+# Initialize the counter for total matches found
+TOTAL_MATCHES_FOUND = 0
+
 # Iterate through the sheets in df that are in the excel_sheets list with headers on row 2
 for sheet_name in excel_sheets:
 
     # Iterate through each OCL source and look for suggestions for the primary and secondary lookups
     for source_name, source_config in automatch_references.items():
         print(f"Looking for suggestions in {source_name} source...")
-        source_filepath = source_config['source_filepath']
+        SOURCE_FILEPATH = source_config['source_filepath']
 
-        with open(source_filepath, 'r', encoding='UTF-8') as f:
+        with open(SOURCE_FILEPATH, 'r', encoding='UTF-8') as f:
             json_data = json.load(f)
-            # Extract only the ID, display names, external IDs, datatype, concept_class, and extras > definitions from the JSON data
+            # Extract only the ID, display names, external IDs, datatype,
+            # concept_class, and extras > definitions from the JSON data
             source_data = []
             for item in json_data:
                 concept_id = item.get('id', '')
                 external_id = item.get('external_id')
                 display_name = item.get('display_name', '')
-                definition = item.get('extras', {}).get('definition', '') if item.get('extras', {}) else ''
-                description = item.get('descriptions', [])[0].get('description', '') if item.get('descriptions', []) else ''
+                definition = (
+                    item.get('extras', {}).get('definition', '')
+                    if item.get('extras', {}) else ''
+                )
+                description = (
+                    item.get('descriptions', [])[0].get('description', '')
+                    if item.get('descriptions', []) else ''
+                )
                 datatype = item.get('datatype')
                 concept_class = item.get('concept_class')
                 concept_url = item.get('url')
@@ -152,54 +171,59 @@ for sheet_name in excel_sheets:
                     'external_id': external_id,
                     'url': concept_url
                 })
-            # Pretty print the display_names
-            #print(json.dumps(display_names, indent=4))
-            #print(f"Loaded {len(display_names)} concepts from {source_name}")
 
         # Load the Excel file, considering the header on row 2
-        df = pd.read_excel(metadata_filepath, sheet_name=sheet_name, header=1)
+        df = pd.read_excel(METADATA_FILEPATH, sheet_name=sheet_name, header=1)
         print(f"Processing sheet: {sheet_name}")
 
         # Load the existing Excel file to append the suggestions
-        book = openpyxl.load_workbook(metadata_filepath)
+        book = openpyxl.load_workbook(METADATA_FILEPATH)
 
-        # Using Excel Writer, append or update the details in the original existing Excel sheet and cells in the specified columns depending on the source name
-        with pd.ExcelWriter(metadata_filepath, engine='openpyxl', mode='a') as writer:
+        # Using Excel Writer, append or update the details in the original existing
+        # Excel sheet and cells in the specified columns depending on the source name
+        with pd.ExcelWriter(METADATA_FILEPATH, engine='openpyxl', mode='a') as writer:
             workbook = writer.book
-            worksheet = workbook[sheet_name]
+            ws = workbook[sheet_name]
 
-            # Get the column indices for the suggestion, external ID, description, datatype, concept class, and extras
-            suggestion_column = find_column_index(worksheet, source_config['suggestion_column'])
-            external_id_column = find_column_index(worksheet, source_config['external_id_column'])
-            description_column = find_column_index(worksheet, source_config['description_column'])
-            datatype_column = find_column_index(worksheet, source_config['datatype_column'])
-            dataclass_column = find_column_index(worksheet, source_config['dataclass_column'])
-            score_column = find_column_index(worksheet, source_config['score_column'])
+            # Get the column indices for the suggestion,
+            # external ID, description, datatype, concept class, and extras
+            suggestion_column = find_column_index(ws, source_config['suggestion_column'])
+            external_id_column = find_column_index(ws, source_config['external_id_column'])
+            description_column = find_column_index(ws, source_config['description_column'])
+            datatype_column = find_column_index(ws, source_config['datatype_column'])
+            dataclass_column = find_column_index(ws, source_config['dataclass_column'])
+            score_column = find_column_index(ws, source_config['score_column'])
 
-            # Iterate through each row in the sheet and get the primary and secondary labels to match
+            # Iterate through each row in the sheet and get
+            # the primary and secondary labels to match
             for index, row in df.iterrows():
                 CONCEPTS_TO_MATCH += 1
                 primary_lookup = row.get('Label if different') or None
                 secondary_lookup = row.get('Question') or row.get('Answers') or None
-                #print(f"Processing row {index+1}: {primary_lookup} - {secondary_lookup}")
 
-                # Get suggestions from each OCL source using closest match with RapidFuzz using the primary lookup and secondary lookup
+                # Get suggestions from each OCL source using closest match
+                # with RapidFuzz using the primary lookup and secondary lookup
                 best_matches = find_best_matches(primary_lookup, secondary_lookup, source_data)
+                TOTAL_MATCHES_FOUND += len(best_matches)
                 # Add the suggestions to the Excel sheet
-                for match in best_matches:
+                for m in best_matches:
                     # Write the suggestion details to the Excel sheet
                     # Add URL concatenated with OCL_URL in Excel cell using =HYPERLINK() formula
-                    worksheet.cell(row=index+3, column=suggestion_column).value = f"=HYPERLINK(\"{OCL_URL}{match[6]}\", \"{match[2]}\")"
-                    worksheet.cell(row=index+3, column=external_id_column).value = match[1]
-                    worksheet.cell(row=index+3, column=description_column).value = match[3]
-                    worksheet.cell(row=index+3, column=datatype_column).value = match[4]
-                    worksheet.cell(row=index+3, column=dataclass_column).value = match[5]
-                    worksheet.cell(row=index+3, column=score_column).value = math.ceil(match[7])
+                    ws.cell(row=index+3, column=suggestion_column).value = (
+                        f"=HYPERLINK(\"{OCL_URL}{m[6]}\", \"{m[2]}\")"
+                    )
+                    ws.cell(row=index+3, column=external_id_column).value = m[1]
+                    ws.cell(row=index+3, column=description_column).value = m[3]
+                    ws.cell(row=index+3, column=datatype_column).value = m[4]
+                    ws.cell(row=index+3, column=dataclass_column).value = m[5]
+                    ws.cell(row=index+3, column=score_column).value = math.ceil(m[7])
 
-                    print(f"Added suggestion: {match[2]} - {match[1]} with score of {match[7]}")
+                    print(
+                        f"Added suggestion: {m[2]} - {m[1]} with score of {m[7]}"
+                    )
 
             # Close the Excel file writer
-            workbook.save(metadata_filepath)
+            workbook.save(METADATA_FILEPATH)
 
 # Count of sources used
 print(f"\nSources used: {len(automatch_references)}")
@@ -209,6 +233,6 @@ CONCEPTS_TO_MATCH = math.ceil(CONCEPTS_TO_MATCH / len(automatch_references))
 print(f"\nTotal concept processed: {CONCEPTS_TO_MATCH}")
 
 # Show the total number of matches found above the threshold
-percentage_found = (MATCHES_FOUND / CONCEPTS_TO_MATCH) * 100
+percentage_found = (TOTAL_MATCHES_FOUND / CONCEPTS_TO_MATCH) * 100
 rounded_percentage_found = math.ceil(percentage_found)
-print(f"\nTotal matches found: {MATCHES_FOUND} ({rounded_percentage_found}%)")
+print(f"\nTotal matches found: {TOTAL_MATCHES_FOUND} ({rounded_percentage_found}%)")
